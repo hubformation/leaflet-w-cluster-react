9718ee89d23ee7d2994d5631c92fdbe360e8b9fb
Benoit Dubuc <benoit.dubuc@hubformation.ca>
2022-08-12 22:10:28 +0000

    Update 4 files

--- src/Clusters.jsx
+++ src/Clusters.jsx
@@ -14,14 +14,28 @@ const customIcon = new L.Icon({
 const MarkerCluster = ({ markers, addMarkers }) => {
   const { map } = useLeaflet();
 
-  useEffect(() => {
+
+  
+    useEffect(() => {
     markerClusters.clearLayers();
-    markers.forEach(({ position }) =>
-      L.marker(new L.LatLng(position.lat, position.lng), {
+    markers.forEach((feature) => {
+        let [lng, lat] = feature.geometry.coordinates;
+        console.log('lat',lat,'long',lng);
+      return L.marker(new L.LatLng(lat, lng), {
         icon: customIcon
       }).addTo(markerClusters)
-    );
-
+    });
+    /*
+    markers.forEach(({ position }) => {
+        let {lat, lng}=position;
+        console.log('lat',lat,'long',lng);
+      return L.marker(new L.LatLng(lat, lng), {
+        icon: customIcon
+      }).addTo(markerClusters)
+    });
+    */
+    
+    
     map.addLayer(markerClusters);
   }, [markers, map]);
 
@@ -31,16 +45,29 @@ const MarkerCluster = ({ markers, addMarkers }) => {
     addMarkers();
     const markersToAdd = [];
     markerClusters.clearLayers();
+    /*
     markers.forEach(({ position }) => {
       const markerToAdd = L.marker(new L.LatLng(position.lat, position.lng), {
         icon: customIcon
-      });
+      }).bindPopup(
+          `<strong>I'm a beautiful pop-up</strong>`);
 
       if (markerToAdd !== undefined) {
         markersToAdd.push(markerToAdd);
       }
     });
+    */
+     markers.forEach((feature) => {
+         let [lng, lat] = feature.geometry.coordinates;
+      const markerToAdd = L.marker(new L.LatLng(lat, lng), {
+        icon: customIcon
+      }).bindPopup(feature.properties.popupContent);
 
+      if (markerToAdd !== undefined) {
+        markersToAdd.push(markerToAdd);
+      }
+    });
+    
     markerClusters.addLayers(markersToAdd);
     const end = window.performance.now();
     console.log(`Time of adding markers and clusters: ${end - start}ms`);
